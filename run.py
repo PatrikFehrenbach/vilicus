f015f2ad3fd60b896e0e48a0825688a8dda5a9ef
Sim4n6 <sim4n6@gmail.com>
2023-07-09 19:43:30 +0100

    revert back

--- run.py
+++ run.py
@@ -19,4 +19,4 @@ scheduler.start()
 print("Scheduler started!")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
